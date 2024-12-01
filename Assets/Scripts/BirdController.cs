9a82370826bdc28d5f63744d1ccaac63566a2c3e
Olga Stroy <olyushka_st@mail.ru>
2024-12-01 22:00:16 +0700

    fix

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -17,6 +17,7 @@ public class BirdController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // нажали пробел - если нет птицы, создаём
         if (curBird is null)
         {
             if (Input.GetKeyDown(KeyCode.Space))
@@ -25,14 +26,16 @@ public class BirdController : MonoBehaviour
             }
         }
 
-        if (Input.GetMouseButtonDown(0))
+        // отпустили ЛКМ - запускаем птицу в полёт и обнуляем переменную
+        if (Input.GetMouseButtonUp(0) && curBird is not null)
         {
             Rigidbody2D rb = curBird.GetComponent<Rigidbody2D>();
             rb.simulated = true;
             rb.AddForce(((transform.position - curBird.transform.position) * 5), ForceMode2D.Impulse);
+            curBird = null;
         }
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && curBird is not null)
         {
             Vector3 v = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0) * Time.deltaTime * magicConst + curBird.transform.position;
             Vector3 newPos = Vector3.ClampMagnitude(v - transform.position, 2);
